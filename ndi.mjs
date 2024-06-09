125cef93e353378ddc08f0c8f92169b26ff0a15d
hopejr <hopejr@gmail.com>
2024-06-09 15:51:16 +1000

    Build and NDI Versions
    - Fixing build scripts for working on x64 but targeting ARM.
    - Updated Linux NDI version to 5.6.1.

--- ndi.mjs
+++ ndi.mjs
@@ -147,17 +147,22 @@ import spawn from "cross-spawn"
     }
     // Spawn build script
     console.log("Preparing to build")
-    if (process.platform === "linux") {
-        spawn.sync("npm", ["run", process.env.npm_config_target_arch === "arm" ? "build:arm" : process.env.npm_config_target_arch === "x64" ? "build:linuxx86" : "build"], {
-	    input: "Native module build required.",
-	    stdio: "inherit"
-        })
-    } else {
-        spawn.sync("npm", ["run", "build"], {
-	    input: "Native module build required.",
-	    stdio: "inherit"
-        })
+    let buildParam = 'build'
+
+    if (process.platform === 'linux') {
+    if (os.arch() !== 'arm' && ['armv7l', 'arm'].includes(process.env.npm_config_target_arch)) {
+        buildParam = 'build:linux-arm'
+    } else if (os.arch() !== 'arm64' && process.env.npm_config_target_arch === 'arm64') {
+        buildParam = 'build:linux-arm64'
+    } else if (os.arch() !== 'x64' && process.env.npm_config_target_arch === 'x64') {
+        buildParam = 'build:linux-x64'
     }
+    }
+
+    spawn.sync('npm', ['run', buildParam], {
+    input: 'Native module build required.',
+    stdio: 'inherit'
+    })
   }
 })().catch((err) => {
     console.log(`** ERROR: ${err}`)
